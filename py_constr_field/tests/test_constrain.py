5ab42ffe65cd7e13c770df46fa2b3e97633011a3
Corentin Cadiou <contact@cphyc.me>
2019-02-04 19:33:20 +0100

    Further test constrains

--- py_constr_field/tests/test_constrain.py
+++ py_constr_field/tests/test_constrain.py
@@ -1,24 +1,75 @@
-from py_constr_field.constrain import Constrain, FieldHandler, DensityConstrain, compute_covariance
-from py_constr_field import filters
-
 from colossus.cosmology import cosmology
 import numpy as np
+from numpy.testing import assert_allclose, assert_array_less
+
+from py_constr_field import constrain as C
+from py_constr_field import filters
+from py_constr_field.field import FieldHandler
 
 cosmo = cosmology.setCosmology('planck18')
-k = np.geomspace(1e-4, 1e4, 1000)
+k = np.geomspace(1e-4, 1e4, 2000)
 Pk = cosmo.matterPowerSpectrum(k)
 
 
 def test_constrain_initialisation():
     filter = filters.GaussianFilter(radius=5)
-    c = Constrain(position=[0, 0, 0], filter=filter, value=1.69,
-                  field_handler=None)
+    c = C.Constrain(position=[0, 0, 0], filter=filter, value=1.69,
+                    field_handler=None)
     c
 
 
-def test_constrain_correlation():
+def test_constrain_correlation_lag():
     fh = FieldHandler(Ndim=3, Lbox=50, dimensions=16, Pk=(k, Pk))
-    filt = filters.TopHatFilter(radius=8)
+    fh.precompute()
+    filt = filters.GaussianFilter(radius=5)
 
-    c = DensityConstrain([0, 0, 0], filt, fh, 1.69)
-    v = compute_covariance(c, c)
+    X1 = np.array([0, 0, 0])
+    X2 = np.array([1, 2, 3])
+    X3 = X2 * 2
+    X4 = X2 * 3
+
+    c1 = C.DensityConstrain(X1, filter=filt, value=1.69, field_handler=fh)
+    c2 = C.GradientConstrain(X2, filter=filt, value=[0, 0, 0], field_handler=fh)
+    c3 = C.HessianConstrain(X3, filter=filt, value=[0, 0, 0, 0, 0, 0], field_handler=fh)
+    c4 = C.ThirdDerivativeConstrain(X4, filter=filt, value=list(range(10)), field_handler=fh)
+
+    fh.add_constrain(c1)
+    fh.add_constrain(c2)
+    fh.add_constrain(c3)
+    fh.add_constrain(c4)
+
+    cov = fh.compute_covariance()
+
+    # Check positive definite
+    evals = np.linalg.eigvalsh(cov)
+    assert_array_less(np.zeros_like(evals), evals)
+
+    # Check symmetry
+    assert_allclose(cov, cov.T)
+
+
+def test_constrain_correlation_nolag():
+    fh = FieldHandler(Ndim=3, Lbox=50, dimensions=16, Pk=(k, Pk))
+    fh.precompute()
+    filt = filters.GaussianFilter(radius=5)
+
+    X1 = np.array([0, 0, 0])
+
+    c1 = C.DensityConstrain(X1, filter=filt, value=1.69, field_handler=fh)
+    c2 = C.GradientConstrain(X1, filter=filt, value=[0, 0, 0], field_handler=fh)
+    c3 = C.HessianConstrain(X1, filter=filt, value=[0, 0, 0, 0, 0, 0], field_handler=fh)
+    c4 = C.ThirdDerivativeConstrain(X1, filter=filt, value=list(range(10)), field_handler=fh)
+
+    fh.add_constrain(c1)
+    fh.add_constrain(c2)
+    fh.add_constrain(c3)
+    fh.add_constrain(c4)
+
+    cov = fh.compute_covariance()
+
+    # Check positive definite
+    evals = np.linalg.eigvalsh(cov)
+    assert_array_less(np.zeros_like(evals), evals)
+
+    # Check symmetry
+    assert_allclose(cov, cov.T)
