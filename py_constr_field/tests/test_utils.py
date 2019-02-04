7506d8f036d1b05ad6d2ea61f3538d8d7207d731
Corentin Cadiou <contact@cphyc.me>
2019-02-04 19:33:10 +0100

    Minor comment

--- py_constr_field/tests/test_utils.py
+++ py_constr_field/tests/test_utils.py
@@ -17,6 +17,7 @@ def test_trapz():
 
 def test_WTH():
     x = np.linspace(0, 2, 1000)
+    # This will show a warning because of a divide by 0
     vref = np.where(x == 0, 1, 3*spherical_jn(1, x) / x)
     vtest = WTH(x)
 
