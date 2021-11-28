cb253b44782116fcd1db0ccf556b361808b7b41d
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-11-28 15:03:59 +0000

    Add 'has_correlations' logic

--- tests/test_constrain.py
+++ tests/test_constrain.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pytest
 from colossus.cosmology import cosmology
-from correlations.correlations import Correlator
-from correlations.utils import Utils
 from numpy.testing import assert_allclose, assert_array_less
 
 from py_constr_field import constrain as C, filters
 from py_constr_field.field import FieldHandler
+
+try:
+    from correlations.correlations import Correlator
+    from correlations.utils import Utils
+
+    HAS_CORRELATIONS = True
+except ImportError:
+    HAS_CORRELATIONS = False
+
 
 cosmo = cosmology.setCosmology("planck18")
 k = np.geomspace(1e-4, 1e4, 2000)
@@ -82,6 +89,9 @@ def test_constrain_correlation_nolag():
     assert_allclose(cov, cov.T)
 
 
+@pytest.mark.skipif(
+    not HAS_CORRELATIONS, reason="Did not install the 'correlations' package"
+)
 def test_full_correlation():
     f1 = filters.GaussianFilter(radius=5)
     f2 = filters.GaussianFilter(radius=6)
@@ -175,6 +185,9 @@ def test_measures():
     assert_allclose(val, tgt)
 
 
+@pytest.mark.skipif(
+    not HAS_CORRELATIONS, reason="Did not install the 'correlations' package"
+)
 def test_xi():
     positions = np.random.rand(10, 3) * 20
 
