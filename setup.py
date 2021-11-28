3909f772cac570615d92d15fca6bc1e8ae7030af
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-11-28 15:17:10 +0000

    Add 'correlation' package

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+from Cython.Build import cythonize
 from setuptools import setup
+from setuptools.extension import Extension
 
-setup()
+try:
+    import cython  # noqa
+    import numpy as np
+
+    include_dirs = [np.get_include(), "correlations"]
+except ImportError:
+    raise ImportError(
+        """Could not import cython or numpy. Building this package from source requires
+cython and numpy to be installed. Please install these packages using
+the appropriate package manager for your python environment."""
+    )
+
+cython_extensions = [
+    Extension(
+        "py_constr_field.correlations.utils",
+        ["py_constr_field/correlations/utils.pyx"],
+        include_dirs=include_dirs,
+    )
+]
+
+setup(ext_modules=cythonize(cython_extensions))
